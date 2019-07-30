ad113cf2a40d5ca9f3b59e9affd6f4b3c50754dd
Ulanbek Zhusupov <diz.dostuk@yandex.ru>
2019-07-30 22:47:42 +0300

    Webpack worked with magical reinstall npm packages

---
+++ avitodumb/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const App = () => {
+  
+  return ( 
+    <div className="container" style={{marginTop: '50px'}}>
+      <div className="row">
+        <div className="col-md-3">
+        <ul class="list-group">
+          <li class="list-group-item">Cras justo odio</li>
+          <li class="list-group-item">Dapibus ac facilisis in</li>
+          <li class="list-group-item">Morbi leo risus</li>
+          <li class="list-group-item">Porta ac consectetur ac</li>
+          <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
+        </div>
+        <div className="col-md-9"></div>
+      </div>
+
+    </div>
+  );
+}
+ 
+export default App;
